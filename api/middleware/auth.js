c4acc80745b201dc13dcb2e7774bccbf23c921e0
SilvesterSpath <spath.sz@gmail.com>
2025-02-21 09:28:38 +0100

    auth middleware

---
+++ api/middleware/auth.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken');
+
+const authenticateJWT = (req, res, next) => {
+  const token = req.header('Authorization');
+  if (!token) return res.status(401).json({ message: 'Access denied' });
+
+  try {
+    const verified = jwt.verify(token.split(' ')[1], process.env.JWT_SECRET);
+    req.user = verified;
+    next();
+  } catch (err) {
+    res.status(403).json({ message: 'Invalid token' });
+  }
+};
+
+module.exports = authenticateJWT;
