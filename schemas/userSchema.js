a36ace6da952ee84ca6939449b2ec8a9ae68b167
SilvesterSpath <spath.sz@gmail.com>
2025-04-17 14:03:53 +0200

    lastname removed

--- schemas/userSchema.js
+++ schemas/userSchema.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema(
   {
     firstname: { type: String, required: true },
-    lastname: { type: String, required: true },
+    lastname: { type: String },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
   },
